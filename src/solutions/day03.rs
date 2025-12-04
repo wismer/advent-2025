289813097e2ea8107ef37e87ed3ab683df870acb
Matt Long <matthewhl@gmail.com>
2025-12-04 16:13:50 -0500

    day 3

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -8,17 +8,30 @@ fn parse_input(data: &str) -> Vec<Vec<usize>> {
 
     banks
 }
-
+// idea is, from left, find largest number UNTIL last item. Keep track of the index of the largest number
+// then, from right, find largest number UNTIL the aforementioned index
 fn select_max_val(batteries: Vec<usize>) -> (usize, usize) {
     let mut first = 0;
+    let mut l_index = 0;
     let mut second = 0;
-    for battery in batteries {
-        println!("battery {}", battery);
-        if battery > first {
-            first = battery;
-        } else if battery > second {
-            second = battery;
+    let mut idx = 0;
+
+    // two loops, check for tenth position first
+    while idx < batteries.len() - 1 {
+        if batteries[idx] > first {
+            first = batteries[idx];
+            l_index = idx;
         }
+
+        idx += 1;
+    }
+
+    while idx > l_index {
+        if batteries[idx] > second {
+            second = batteries[idx];
+        }
+
+        idx -= 1;
     }
 
     (first, second)
@@ -41,10 +54,51 @@ pub fn solve_part_one(data: &str) -> usize {
     count
 }
 
-pub fn solve_part_two(data: &str) -> usize {
-    // Implement the logic for part two of the puzzle here
-    0
+fn select_max_val_p2(batteries: Vec<usize>) -> usize {
+    let mut idx: usize = 0;
+    let mut spares = batteries.len() - 12;
+    let mut buff = String::new();
+
+    while buff.len() < 12 {
+        let range = idx..(idx + spares + 1);
+        let window = &batteries[range];
+        let max_val = window.iter().max().unwrap();
+        let (i, n) = window
+            .iter()
+            .enumerate()
+            .find(|n| n.1 == max_val).unwrap();
+        buff.push_str(&n.to_string());
+
+        if i > 0 {
+            if spares != 0 {
+                spares -= i;
+            }
+            idx += i + 1;
+        } else {
+            idx += 1;
+        }
+    }
+
+
+
+    usize::from_str_radix(&buff, 10).unwrap()
 }
+
+
+pub fn solve_part_two(data: &str) -> usize {
+    // going to be similar to part one, 
+    let mut count = 0;
+    let banks = parse_input(data);
+    for bank in banks {
+        let line: Vec<String> = bank.iter().map(|x| x.to_string()).collect();
+        let b = line.join("");
+        let max_val = select_max_val_p2(bank);
+        count += max_val;
+    }
+
+    count
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -58,13 +112,26 @@ mod tests {
 818181911112111";
         assert_eq!(solve_part_one(data), 357);
     }
-
+    // [818] 181911112111
+    // 8 [181] 1911112111
+    // 88 [18] 1911112111
+    // 888911112111
+    // 888911112111 
+    // [8181]81911112111 -> 8
+    // 8[1818]1911112111 -> 888
+    // 888[191]1112111 -> 8889
+    // 8889[11]12111 -> 88891
+    // 88891[11]2111 -> 888911
+    // 888911[12]111 -> 888912
+    // 
+    //  xx4[234]234234278 (1 space left)
+    // 4334234
     #[test]
     fn test_solve_part_two() {
         let data = "987654321111111
 811111111111119
 234234234234278
 818181911112111";
-        assert_eq!(solve_part_two(data), 0);
+        assert_eq!(solve_part_two(data), 3121910778619);
     }
 }
